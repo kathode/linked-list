3ffb5885c50ec91d33ab0c59acf436124792b335
Kathode <work.qiyan@gmail.com>
2025-03-26 14:55:59 +1100

    Add extra checks to insertAt method
    - ensure insertions can be added to head and tail
    - ensure insertions cannot be added when out of range

--- src/linkedList.js
+++ src/linkedList.js
@@ -67,10 +67,22 @@ export class LinkedList {
     const nextNode = this.list[index];
     const newNode = new Node(value, nextNode);
 
-    this.list.splice(index, 0, newNode);
+    const isHead = index === 0;
+    const isTail = this.size() === index;
+    const outOfRange = index > this.size();
 
-    if (this.list.length > 1) {
+    if (this.size() < 1) {
+      return;
+    } else if (isHead) {
+      this.list.splice(index, 0, newNode);
+    } else if (isTail) {
       this.list[index - 1].nextNode = newNode;
+      this.list.splice(index, 0, new Node(value));
+    } else if (outOfRange) {
+      console.log("out of range error");
+    } else {
+      this.list[index - 1].nextNode = newNode;
+      this.list.splice(index, 0, newNode);
     }
   }
 }
